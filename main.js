a2925bd9fe58839b87066a4cc05498858ba9784c
doctor-rutvik14 <rutvik14d@gmail.com>
2020-07-12 21:33:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+var btnCopy = document.getElementById('btnCopy');
+var password = document.getElementById('password');
+var result = '';
+document.write(password);
+var a ;
+function copy(){
+  if(result == ''){
+    document.getElementById('alert1').style.display='';
+    setTimeout(function(){
+      document.getElementById('alert1').style.display='none';
+    },3000);
+  }
+  else{
+    password.select();
+    document.execCommand('Copy');
+    document.getElementById('alert').style.display='';
+    setTimeout(function(){
+      document.getElementById('alert').style.display='none';
+    },3000);
+  }
+};
+function getRandomArbitrary() {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-={}:;"<>.,/?';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 17; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    document.getElementById('password').value = result; 
+}
+
